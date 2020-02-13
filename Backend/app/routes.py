0024518828a0191e4951864286789e438a993c6c
KrampusCo <krampuscompany@gmail.com>
2020-02-13 17:31:55 +0700

    Add registration api

--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,8 +1,8 @@
 from app import app
 from flask import Flask, jsonify, request
-from flask_login import current_user, login_user
 from app.models import User
 from flask_cors import CORS
+from app import db
 
 CORS(app)
 
@@ -18,12 +18,37 @@ def index():
     return "Hello, World!"
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/api/login', methods=['GET', 'POST'])
 def login():
     form = request.form
-    if current_user.is_authenticated:
-        return request.data
     user = User.query.filter_by(username=form['username']).first()
     if user is None or not user.check_password(form['password']):
         return jsonify("bb")
     return jsonify("yes")
+
+
+@app.route('/api/logout')
+def logout():
+    return jsonify("logout")
+
+
+@app.route('/api/register', methods=["POST"])
+def register():
+    data = request.form
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+    try:
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+    except error:
+        return jsonify({
+            "status": "error",
+            "message": "Could not add user"
+        })
+
+    return jsonify({
+        "status": "success",
+        "message": "User added successfully"
+    }), 201
